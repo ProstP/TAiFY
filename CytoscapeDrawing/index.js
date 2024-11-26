9fd28c4db16ce559dc9490c4a6236de18a6b1e2c
LAPTOP-U0R8E398\prost <prosto_channal@mail.ru>
2024-11-26 08:52:52 +0300

    Доделал визуализацию

--- CytoscapeDrawing/index.js
+++ CytoscapeDrawing/index.js
@@ -1,24 +1,71 @@
-const DrawGraph = () => {
-  let options = {
-    container: document.getElementById("cy"),
+const OpenAndDrawGraph = (e) => {
+  const text = ReadDataFromFile(e, (text) => {
+    const graph = CreateGraph(text);
+    console.log(graph);
+    DrawGraph(graph);
+  });
+};
+
+const CreateGraph = (text) => {
+  const lines = text.split("\r\n");
+
+  const firstLine = lines[0].split(";");
+  console.log(firstLine);
+
+  const states = [];
+
+  for (i = 0; i < firstLine.length; i++) {
+    if (firstLine[i] !== "") {
+      states.push(firstLine[i]);
+    }
+  }
+
+  const edges = [];
+  for (i = 1; i < lines.length; i++) {
+    const line = lines[i].split(";");
+
+    const inSymbol = line[0];
+
+    for (j = 1; j < line.length; j++) {
+      if (line[j] === "-") {
+        continue;
+      }
+
+      edges.push({
+        data: inSymbol,
+        from: states[j - 1],
+        to: line[j],
+      });
+    }
+  }
+
+  return {
+    states,
+    edges,
+  };
+};
+
+const ReadDataFromFile = (e, processText) => {
+  if (e.target.files === null) {
+    return;
+  }
+
+  const file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    processText(e.target.result);
   };
 
-  const elements = [
-    { data: { id: "a" } },
-    { data: { id: "b" } },
-    { data: { id: "s0" } },
-    { data: { id: "s1" } },
-    { data: { data: "z0", source: "a", target: "b" } },
-    { data: { data: "z1", source: "a", target: "a" } },
-    { data: { data: "z1", source: "a", target: "a" } },
-    { data: { source: "a", target: "s0" } },
-    { data: { source: "s0", target: "s1" } },
-    { data: { source: "s1", target: "a" } },
-  ];
+  reader.readAsText(file);
+};
 
-  options = {
-    ...options,
-    elements: elements,
+const DrawGraph = (graph) => {
+  let options = {
+    container: document.getElementById("cy"),
   };
 
   const nodeStyle = {
@@ -49,6 +96,41 @@ const DrawGraph = () => {
     ...options,
     style: [nodeStyle, arrowStyle],
     layout: layoutStyle,
+  };
+
+  // const elements = [
+  //   { data: { id: "a" } },
+  //   { data: { id: "b" } },
+  //   { data: { id: "s0" } },
+  //   { data: { id: "s1" } },
+  //   { data: { data: "z0", source: "a", target: "b" } },
+  //   { data: { data: "z1", source: "a", target: "a" } },
+  //   { data: { data: "z1", source: "a", target: "a" } },
+  //   { data: { source: "a", target: "s0" } },
+  //   { data: { source: "s0", target: "s1" } },
+  //   { data: { source: "s1", target: "a" } },
+  // ];
+
+  let elements = [];
+
+  for (i = 0; i < graph.states.length; i++) {
+    elements.push({
+      data: { id: graph.states[i] },
+    });
+  }
+  for (i = 0; i < graph.edges.length; i++) {
+    elements.push({
+      data: {
+        data: graph.edges[i].data,
+        source: graph.edges[i].from,
+        target: graph.edges[i].to,
+      },
+    });
+  }
+
+  options = {
+    ...options,
+    elements: elements,
   };
 
   const cy = cytoscape(options);
@@ -85,4 +167,8 @@ const DrawGraph = () => {
   //   });
 };
 
-window.onload = DrawGraph;
+window.onload = () => {
+  const input = document.getElementById("input");
+
+  input.onchange = OpenAndDrawGraph;
+};
